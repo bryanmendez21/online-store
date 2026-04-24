7e45e06211f732b10b39a5901527e972801d21f3
bryan <bryanmendez21@icloud.com>
2026-04-24 09:57:23 -0400

    added switch statements to navigate my display screen and added a do while loop so customers can go back to the main screen

--- src/main/java/com/pluralsight/OnlineStore.java
+++ src/main/java/com/pluralsight/OnlineStore.java
@@ -5,13 +5,40 @@ import java.util.Scanner;
 public class OnlineStore {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        System.out.println("----------Welcome to The Store----------");
-        System.out.println("(1) Display All Products");
-        System.out.println("(2) Display Cart");
-        System.out.println("(0) Exit");
-        System.out.print("Pick from the options above by entering a numerical value: ");
-        int userOption = userInput.nextInt();
-        userInput.nextLine();
+
+        String goBack;
+
+        do{
+            System.out.println("----------Welcome to The Store----------");
+            System.out.println("(1) Display All Products");
+            System.out.println("(2) Display Cart");
+            System.out.println("(0) Exit");
+            System.out.print("Pick from the options above by entering a numerical value: ");
+            int userOption = userInput.nextInt();
+            userInput.nextLine();
+
+            switch (userOption){
+                case 1 -> {
+                    System.out.println("All Products");
+                }
+                case 2 ->{
+                    System.out.println("Cart");
+                }
+                case 0 ->{
+                    break;
+
+                }
+            }
+
+
+            System.out.print("Would you want to go back (yes/no): ");
+            goBack = userInput.nextLine();
+
+        }
+        while (goBack.equalsIgnoreCase("yes"));
+
+        System.out.println("Good Bye");
+        userInput.close();
 
     }
 
