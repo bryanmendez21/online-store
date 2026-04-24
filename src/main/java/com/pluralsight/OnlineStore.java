db210af450dddaafba3e3f5bfa60f8889f7b7c79
bryan <bryanmendez21@icloud.com>
2026-04-24 11:19:33 -0400

    added a switch statement inside my first case switch to ask users to view all products or search for product.

--- src/main/java/com/pluralsight/OnlineStore.java
+++ src/main/java/com/pluralsight/OnlineStore.java
@@ -3,15 +3,19 @@ package com.pluralsight;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class OnlineStore {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner userInput = new Scanner(System.in);
-        BufferedReader bufReader = new BufferedReader(new FileReader("src/main/resources/products.csv"));
 
+
+        // For the do while loop
         boolean running = true;
         String goBack;
+
 
         do{
             System.out.println("----------Welcome to The Store----------");
@@ -24,9 +28,29 @@ public class OnlineStore {
 
             switch (userOption){
                 case 1 -> {
+                    System.out.println("----- Product Display-----");
+                    System.out.println("(1) See all available products");
+                    System.out.println("(2) looking for specific product");
+                    System.out.println("(3) Go back to main menu");
+                    System.out.print("Pick using numerical values: ");
+                    int userOptionDisplay = userInput.nextInt();
+                    userInput.nextLine();
 
-                    System.out.println(bufReader);
-                    System.out.println("All Products");
+                    switch (userOptionDisplay){
+                        case 1 -> AllProducts();
+                        case 2 ->{
+                            System.out.print("what product are you looking for: ");
+                            String productSearch = userInput.nextLine();
+
+                        }
+                        case 3 -> {
+                            continue;
+                        }
+                        default ->{
+                            System.out.println("Invalid Option");
+                        }
+                    }
+
                     break;
                 }
                 case 2 ->{
@@ -56,6 +80,24 @@ public class OnlineStore {
         System.out.println("Good Bye");
         userInput.close();
 
+    }
+
+    // Methods
+    public static void AllProducts() {
+        try {
+            BufferedReader bufReader = new BufferedReader(new FileReader("src/main/resources/products.csv"));
+            // For the file reader
+            String allPro;
+
+            System.out.println("Display all products");
+            while ((allPro = bufReader.readLine()) != null) {
+                System.out.println(allPro);
+            }
+            bufReader.close();
+
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
     }
 
 }
